756c3a072e04fd9e089de89219cdf02677ce11e7
halida <linjunhalida@gmail.com>
2014-07-10 17:27:32 +0800

    update

---
+++ ruby/rdoc/test.rb
@@ -0,0 +1,36 @@
+# a test class, blablabla...
+class Test
+
+  # work for doing
+  #  Test.work(data: 12)
+  def self.work(opt)
+  end
+
+  # seperate documents
+  # * first
+  # * second
+  def sep(opt)
+    for o in opt
+      dosth(o)
+    end
+  end
+
+  # count the milk
+  # [cat] small
+  # check http://g.cn
+  def count
+    return 12
+  end
+
+  def fred
+    yield line, address
+  end
+
+  # Run sth
+  # Params:
+  # +command+:: what I'm doning is right.
+  # outdate:: yes, it is true.
+  #           and I'm _italic_, *bold*, and +typewriter+
+  def sth(opt)
+  end
+end
