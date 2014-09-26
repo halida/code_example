49f768ec2e6cbeb82aaf541cb762193d59ab8def
halida <linjunhalida@gmail.com>
2014-09-26 14:10:25 +0800

    csv use header

--- ruby/csv/run.rb
+++ ruby/csv/run.rb
@@ -5,11 +5,24 @@ filename = "tmp/file.csv"
 
 # write
 CSV.open(filename, "wb+") do |csv|
-  csv << ["row", "of", "CSV", "data"]
-  csv << ["another", "row"]
+  csv << ["ID", "name", "value"]
+  csv << [1, "halida", 120]
+  csv << [2, "James", 300]
 end
 
+# read as table
+c = CSV.new(File.open(filename), headers: :first_row)
+# read first
+row = c.readline
+# read all
+c.read
 
+puts "ID: #{row['ID']}"
+# return all fields
+puts "first line: #{row.fields}"
+
+# read all
 CSV.foreach(filename) do |row|
-  puts row.to_s
+  row.to_s
 end
+
