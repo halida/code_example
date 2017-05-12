359e1391808ff4a62db6dcf81895a7ec0ec45553
halida <linjunhalida@gmail.com>
2017-05-12 19:21:50 +0800

    update

---
+++ csharp/dotnet_core_another/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_core_another
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
