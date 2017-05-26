f8cf2b1eaeeee090df04a3ba0ccb10abe93939b0
halida <linjunhalida@gmail.com>
2017-05-26 10:21:04 +0800

    update

---
+++ java/IdeaHello/src/com/uestc/Main.java
@@ -0,0 +1,22 @@
+package com.uestc;
+
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("hello");
+        list.add("this is an test");
+        for(String s : list) {
+            System.out.println(s);
+        }
+
+        workingOut();
+    }
+
+    public static void workingOut() {
+        // hello
+    }
+}
