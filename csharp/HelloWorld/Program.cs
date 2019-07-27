ca91e3c84563f0d8d982b4c2e808d67d097556b8
halida <linjunhalida@gmail.com>
2019-07-27 16:56:46 +0800

    dotnet core helloworld

---
+++ csharp/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic; 
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>(){1,2,3};
+            foreach(var i in list){
+                Console.Write(i);
+                Console.Write(",");
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
