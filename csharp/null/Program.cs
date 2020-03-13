b6408b6e22d201c89236f830900c3f0b6e6ce075
halida <linjunhalida@gmail.com>
2020-03-13 15:00:37 +0800

    add null example

---
+++ csharp/null/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Null
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Work(null);
+        }
+
+        static void Work(string? name)
+        {
+            if (name == null) { return; }
+            Console.WriteLine(name.Length);
+        }
+    }
+}
