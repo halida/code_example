02bd412a9772a143192cd7e55e08d4aa53c81b31
halida <linjunhalida@gmail.com>
2020-03-14 09:39:09 +0800

    events

---
+++ csharp/events/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace events
+{
+    public class SampleEventArgs
+    {
+        public SampleEventArgs(string s) { Text = s; }
+        public String Text { get; } // readonly
+    }
+
+    public class Publisher
+    {
+        public event EventHandler<SampleEventArgs> SampleEvent;
+
+        public virtual void RaiseSampleEvent()
+        {
+            // Raise the event in a thread-safe manner using the ?. operator.
+            SampleEvent?.Invoke(this, new SampleEventArgs("Hello"));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var pub = new Publisher();
+            pub.SampleEvent += (t, e) => Console.WriteLine($"on event: {e.Text}");
+            pub.RaiseSampleEvent();
+        }
+    }
+}
